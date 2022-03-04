05ab16cd06989b7972d39705f48024ecc005a6ca
YaBoiSpy7 <99022983+YaBoiSpy7@users.noreply.github.com>
2022-03-04 12:07:29 -0500

    23 Feb 2022

--- src/lo2_oop/objects/Player.scala
+++ src/lo2_oop/objects/Player.scala
@@ -1,31 +1,21 @@
 package lo2_oop.objects
 
-class Player(var xLocation: Double, var yLocation: Double, val maxHitPoints: Int) {
+class Player (var xLoc1:Double,var yLoc1:Double,
+              var maxHitPoint:Int)
+      extends GameObject (xLoc1,yLoc1){
+  var health:Int=maxHitPoint
+  var damage:Int=4
 
-  var hp: Int = this.maxHitPoints
-  val damage: Int = 4
-
-  def takeDamage(damage: Int): Unit = {
-    this.hp -= damage //called as eg. Player1.takeDamage. "this" takes the damage from the player calling the method
+  def takeDamage(damage:Int):Unit={
+    this.health-=damage
   }
-
-  def attack(otherPlayer: Player): Unit ={
+  def attack(otherPlayer:Player):Unit={
     otherPlayer.takeDamage(this.damage)
   }
 
-  def conscious(): Boolean = {
-    this.hp > 0
-  }
-
-  def move(dx: Double, dy: Double): Unit = {
-    this.xLocation + dx
-    this.yLocation + dy
-  }
-
   override def toString():String={
-    var out:String="xLocation: "+this.xLocation
-    out+=" yLocation: " +this.yLocation
-    out+="health "+this.hp+"/"+this.maxHitPoints
+    var out:String=super.toString
+    out+=" health "+this.health+"/"+this.maxHitPoint
     out
   }
 
--- src/lo2_oop/objects/Point2D.scala
+++
@@ -1,5 +0,0 @@
-package lo2_oop.objects
-
-class Point2D(var x: Double, var y: Double) {
-
-}
