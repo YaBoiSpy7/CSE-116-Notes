bcf139c0d674837b9826fdde80fb4299341ad040
YaBoiSpy7 <99022983+YaBoiSpy7@users.noreply.github.com>
2022-04-28 10:33:07 -0400

    27 Apr 2022

--- src/lo4_data_structures/Queue.scala
+++ src/lo4_data_structures/Queue.scala
@@ -1,28 +1,46 @@
 package lo4_data_structures
 
-class Queue[A] {
-  var front:LinkedListNode[A] = null
-  var back:LinkedListNode[A] = null
-
+class Queue [A]{
+  var front:LinkedListNode[A]=null
+  var back:LinkedListNode[A]=null
   //enqueue
-  def enqueue(element:A): Unit ={
-    if (this.back == null){
-      this.back = new LinkedListNode[A](element, null)
-      this.front == this.back
-    }
-    else{
-      this.back.next = new LinkedListNode[A](element, null)
-      this.back = this.back.next
+  def enqueue(element:A):Unit={
+    if (this.back==null){
+      this.back=new LinkedListNode[A](element,null)
+      this.front=this.back
+    } else {
+      this.back.next=new LinkedListNode[A](element,
+        null)
+      this.back=this.back.next
     }
   }
   //dequeue
-  def dequeue(): A ={
-    val toReturn = this.front.value
-    this.front = this.front.next
-    if (this.front == null){
-      this.back = null
+  def dequeue():A={
+    val toReturn=this.front.value
+    this.front=this.front.next
+    if (this.front==null){
+      this.back=null
     }
-    return toReturn
+    toReturn
+  }
+  def empty():Boolean={
+    this.front==null
+  }
+  def contains(a:A):Boolean={
+    if (this.front==null){
+      false
+    } else if (null==this.front.find(a)){
+      false
+    } else {
+      true
+    }
   }
 
+  override def toString: String = {
+    if (this.front==null){
+      ""
+    } else {
+      this.front.toString
+    }
+  }
 }
