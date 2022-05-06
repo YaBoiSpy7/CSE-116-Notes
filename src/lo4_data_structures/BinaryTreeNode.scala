a8a7fa69f482b2ac0df4bb90defb4d710b0857fa
YaBoiSpy7 <99022983+YaBoiSpy7@users.noreply.github.com>
2022-05-06 09:14:20 -0400

    6 May 2022

--- src/lo4_data_structures/BinaryTreeNode.scala
+++ src/lo4_data_structures/BinaryTreeNode.scala
@@ -45,19 +45,40 @@ class BinaryTreeNode[B](var value:B, var left:BinaryTreeNode[B], var right:Binar
     }
     out
   }
+
+  def lo4Test[B](node:BinaryTreeNode[B]): Unit={
+    if (node != null){
+      if (node.left != null){
+        lo4Test(node.left)
+      } else{
+        println("left")
+      }
+      if (node.right != null){
+        lo4Test(node.right)
+      } else{
+        println("right")
+      }
+      println(node.value)
+    }
+  }
 }
+
+
 object runBTN{
   def main(args: Array[String]): Unit = {
-    var root=new BinaryTreeNode[Int](5,null,null)
-    root.left=new BinaryTreeNode[Int](2,null,null)
-    root.right=new BinaryTreeNode[Int](8,null,null)
-    root.left.left=new BinaryTreeNode[Int](-1,null,null)
-    root.left.right=new BinaryTreeNode[Int](3,null,null)
-    root.right.left=new BinaryTreeNode[Int](6,null,null)
-    root.right.right=new BinaryTreeNode[Int](9,null,null)
 
-    println(root.preOrderTraversal(root))
-    println(root.inOrderTraversal(root))
-    println(root.postOrderTraversal(root))
+
+    var root=new BinaryTreeNode[Int](0,null,null)
+    root.left=new BinaryTreeNode[Int](-4,null,null)
+    root.right=new BinaryTreeNode[Int](3,null,null)
+    //root.left.left=new BinaryTreeNode[Int](-1,null,null)
+    root.left.right=new BinaryTreeNode[Int](-1,null,null)
+    //root.right.left=new BinaryTreeNode[Int](6,null,null)
+    //root.right.right=new BinaryTreeNode[Int](9,null,null)
+
+    //println(root.preOrderTraversal(root))
+    //println(root.inOrderTraversal(root))
+    //println(root.postOrderTraversal(root))
+    root.lo4Test(root)
   }
 }
